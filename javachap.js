94b45cc71bb3bd757963df1c288757bb4df0b34a
Hrag17 <58171813+Hrag17@users.noreply.github.com>
2020-01-20 15:13:49 +0400

    Add files via upload

---
+++ javachap.js
@@ -0,0 +1,24 @@
+var a=prompt("insert your age below");
+a=parseInt(a);
+
+if(a<10)
+	
+	{
+		
+		document.write("Under-Aged");
+	}
+else if (a>9 && a<18)
+{
+
+document.write("Middle-Aged");
+
+}	
+else
+{
+	document.write("Over-Aged");
+}
+
+
+
+
+
